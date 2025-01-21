3298aa416ae8fb77afcdd46ab6069855fe12a7b9
Senhor_Ini <rafabs26126@gmail.com>
2025-01-21 19:03:53 -0300

    Create exe34.cpp

---
+++ Exercícios/exe34.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <windows.h>
+
+void printInfo(const std::string nome, const int idade) {       // Esse código resulta em erro porque já declara as variáveis nome e idade como constantes
+    nome = " ";                                                 // Portanto, alterar elas como é feito aqui, colide com a lógica do resto do código
+    idade = 0;
+
+    std::cout << nome << '\n';
+    std::cout << idade << '\n';
+}
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    std::string nome = "Rafael";
+    int idade = 18;
+
+    printInfo(nome, idade);
+
+    return 0;
+}
