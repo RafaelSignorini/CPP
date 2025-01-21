f9067fe256f9d45e11de0a6aa7ba546d019b2efe
Senhor_Ini <rafabs26126@gmail.com>
2025-01-21 18:29:26 -0300

    Create exe05.cpp

---
+++ Exercícios/exe05.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    double x = (int) 3.14;              // Conversão explícita de um double para um int
+    std::cout << x << '\n';
+
+    std::cout << (char) 100 << '\n';    // Conversão implícita de um double para um int
+
+    int corretas = 8;
+    int questoes = 10;
+    double nota = corretas / (double) questoes * 100;
+
+    std::cout << nota << "%\n";
+
+    return 0;
+}
