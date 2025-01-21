a6d7edeb89a74308f4f9ad2515f0be825745b837
Senhor_Ini <rafabs26126@gmail.com>
2025-01-21 18:25:39 -0300

    Create exe03.cpp

---
+++ Exercícios/exe03.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+typedef std::string text_t;
+using num_t = int;
+
+int main() {
+
+    text_t nome = "Rafael";
+    num_t idade = 18;
+
+    std::cout << nome << '\n';
+    std::cout << idade << '\n';
+
+    return 0;
+}
