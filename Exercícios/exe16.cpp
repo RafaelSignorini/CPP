f4ce68a5b81248fb8e1fed2650abfdc23b16f964
Senhor_Ini <rafabs26126@gmail.com>
2025-01-21 18:46:48 -0300

    Create exe16.cpp

---
+++ Exercícios/exe16.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <windows.h>
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    
+    int fileiras;
+    int colunas;
+    char simbolo;
+
+    std::cout << "Quantas fileiras? ";
+    std::cin >> fileiras;
+
+    std::cout << "Quantas colunas? ";
+    std::cin >> colunas;
+
+    std::cout << "Insira um símbolo: ";
+    std::cin >> simbolo;
+    
+    for (int i = 1; i <= fileiras; i++) {       // O laço delimita a quantidade de fileiras a serem criadas baseadas no input na linha 12
+        for (int j = 1; j <= colunas; j++){     // Enquanto esse laço delimita a quantidade de colunas baseadas no input na linha 15
+        std::cout << simbolo;                   // O "símbolo" seria um caracter que o usuário inseriu para ser repetido até completas as colunas e fileiras
+        }
+        std::cout << '\n';
+    }
+
+    return 0;
+}
