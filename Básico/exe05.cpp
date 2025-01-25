ea2fe1f1906df20de874e571b18a6770c5cc656b
Senhor_Ini <rafabs26126@gmail.com>
2025-01-25 12:41:55 -0300

    Update exe05.cpp

--- Básico/exe05.cpp
+++ Básico/exe05.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
 int main() {
-    double x = (int) 3.14;              // Conversão explícita de um double para um int
+    double x = (int) 3.14; // Conversão explícita de um double para um int
     std::cout << x << '\n';
 
-    std::cout << (char) 100 << '\n';    // Conversão implícita de um double para um int
+    std::cout << (char) 100 << '\n'; // Conversão implícita de um double para um int
 
     int corretas = 8;
     int questoes = 10;
