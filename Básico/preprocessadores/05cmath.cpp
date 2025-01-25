7f67c4efa480ed3db46333eee87ffbead39581ce
Senhor_Ini <rafabs26126@gmail.com>
2025-01-25 11:51:29 -0300

    correção de erros mínimos

--- Básico/preprocessadores/05cmath.cpp
+++ Básico/preprocessadores/05cmath.cpp
@@ -5,7 +5,7 @@ int main() {
     int base = 5, expoente = 3; // base é uma variável que armazena 5, expoente é uma variável que armazena 3
     int potencia = pow(base, expoente); // pow é uma função que retorna a potência de um número, potencia é uma variável que armazena a potência de base elevado a expoente
 
-    std::cout << base << " elevado a " << expoente << "equivale a " << potencia << '\n';
+    std::cout << base << " elevado a " << expoente << " equivale a " << potencia << '\n';
 
     return 0;
 }
