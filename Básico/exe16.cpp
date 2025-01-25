dd62933898a40de953d2454dd0b57bdee063dea8
Senhor_Ini <rafabs26126@gmail.com>
2025-01-25 20:55:41 -0300

    Update exe16.cpp

--- Básico/exe16.cpp
+++ Básico/exe16.cpp
@@ -18,9 +18,9 @@ int main() {
     std::cout << "Insira um símbolo: ";
     std::cin >> simbolo;
     
-    for (int i = 1; i <= fileiras; i++) {       // O laço delimita a quantidade de fileiras a serem criadas baseadas no input na linha 12
-        for (int j = 1; j <= colunas; j++){     // Enquanto esse laço delimita a quantidade de colunas baseadas no input na linha 15
-        std::cout << simbolo;                   // O "símbolo" seria um caracter que o usuário inseriu para ser repetido até completas as colunas e fileiras
+    for (int i = 1; i <= fileiras; i++) { // O laço delimita a quantidade de fileiras a serem criadas baseadas no input na linha 12
+        for (int j = 1; j <= colunas; j++){ // Enquanto esse laço delimita a quantidade de colunas baseadas no input na linha 15
+        std::cout << simbolo; // O "símbolo" seria um caracter que o usuário inseriu para ser repetido até completas as colunas e fileiras
         }
         std::cout << '\n';
     }
