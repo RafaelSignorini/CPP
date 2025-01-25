9dfcbd6598472cfc65b830b31d6397c0fb82f248
Senhor_Ini <rafabs26126@gmail.com>
2025-01-25 20:33:50 -0300

    Update exe14.cpp

--- Básico/exe14.cpp
+++ Básico/exe14.cpp
@@ -5,13 +5,13 @@ int main() {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    for (int i = 1; i <= 10; i++) {     // crescente
+    for (int i = 1; i <= 10; i++) { // crescente
         std::cout << i << '\n';
     }
 
     std::cout << "feliz ano novo\n";
     
-    for (int i = 10; i >= 1; i--) {     // decrescente
+    for (int i = 10; i >= 1; i--) { // decrescente
         std::cout << i << '\n';
     }
 
