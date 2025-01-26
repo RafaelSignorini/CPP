fa536820cc21242701f6f94212c366568c3366ef
Senhor_Ini <rafabs26126@gmail.com>
2025-01-25 22:29:02 -0300

    Update exe19.cpp

--- Básico/exe19.cpp
+++ Básico/exe19.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <windows.h>
 
-void bomdia(std::string nome, int idade) {                          // O que fica dentro do parênteses é o parâmetro para receber as variáveis de fora da função
-    std::cout << "Bom dia, " << nome << '\n';                       // A função declara a variável como se fosse um output de texto comum na main(), porém
-    std::cout << "Sua idade registrada é " << idade << " anos\n";   // tendo que especificá-la nos parâmetros da função
+void bomdia(std::string nome, int idade) { // o que fica dentro do parênteses é o parâmetro para receber as variáveis de fora da função
+    std::cout << "Bom dia, " << nome << '\n'; // a função declara a variável como se fosse um output de texto comum na main(), porém
+    std::cout << "Sua idade registrada é " << idade << " anos\n"; // tendo que especificá-la nos parâmetros da função
 }
 
 int main() {
@@ -13,7 +13,7 @@ int main() {
     std::string nome = "Rafael";
     int idade = 18;
 
-    bomdia(nome, idade);                                            // A função recebe a variável 'nome' para ser usada da maneira especificada pela função que a chama
+    bomdia(nome, idade); // a função recebe a variável 'nome' para ser usada da maneira especificada pela função que a chama
 
     return 0;
 }
