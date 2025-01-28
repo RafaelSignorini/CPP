0b34b1be5a50b40041d9407d63e8cd38874ffb16
Senhor_Ini <rafabs26126@gmail.com>
2025-01-28 12:10:01 -0300

    Create 04matriz.cpp

---
+++ Básico/variaveis/listas/04matriz.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <windows.h>
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    std::string pessoas[3][3] = {{"Jorge", "Majé", "Vinicin"}, // isso é uma lista de listas, ou seja, uma matriz
+                                {"Henry", "Matheus", "Peaga"},
+                                {"Rafael", "Tales", "Malu"}};
+
+    std::cout << pessoas[0][0] << " "; // esse é um jeito mais simples de acessar um valor de uma matriz
+    std::cout << pessoas[2][1] << " ";
+    std::cout << pessoas[0][1] << " ";
+    std::cout << std::endl; // adicionado para pular linha e melhorar a visualização dos dados
+
+    int linhas = sizeof(pessoas)/sizeof(pessoas[0]);
+    int colunas = sizeof(pessoas)/sizeof(pessoas[0][0]);
+
+    for (int i = 0; i < linhas; i++) { // enquanto esse é um jeito mais complexo de acessar um valor de uma matriz, porém esse é mais eficiente e abrange todos os casos
+        for (int j = 0; j < colunas; j++) {
+            std::cout << pessoas[i][j] << " ";
+        }
+    }
+
+    return 0;
+}
