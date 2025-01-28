9291de08e51f2aeab96e2171f9317f5c58ea7429
Senhor_Ini <rafabs26126@gmail.com>
2025-01-28 11:27:54 -0300

    Update exe26.cpp

--- Básico/exe26.cpp
+++ Básico/exe26.cpp
@@ -5,7 +5,7 @@ int main() {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    std::string nomes[] = {"Rafael", "Stéfany", "Lilith", "Zara"};
+    std::string nomes[] = {"Rafael", "Stéfany", "Lilith", "Zara"}; // aqui atribuímos os valores à lista, e o compilador já sabe o tamanho da lista baseado em quantas variáveis, sempre de mesmo tipo, foram atribuídas
     int notas[] = {6, 3, 9, 6, 8};
 
     for (int i = 0; i < sizeof(nomes)/sizeof(std::string); i++) {
