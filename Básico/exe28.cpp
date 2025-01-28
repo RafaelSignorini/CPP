b7995acabbe733a9c8e264fb50ed6c23ae70f9b9
Senhor_Ini <rafabs26126@gmail.com>
2025-01-28 11:54:32 -0300

    Update exe28.cpp

--- Básico/exe28.cpp
+++ Básico/exe28.cpp
@@ -4,13 +4,13 @@
 int localizarArray(int array[], int tamanho, int elemento) {
     for (int i = 0; i < tamanho; i++) {
         if (array[i] == elemento) {
-            return i;
+            return i; // se o elemento for encontrado, a posição dele na lista é retornada
         }
     }
-    return -1;
+    return -1; // se o elemento não for encontrado, -1 é retornado
 }
 
-int localizarComida(std::string array[], int tamanho, std::string elemento) {
+int localizarComida(std::string array[], int tamanho, std::string elemento) { // ambas as funções são iguais, com a diferença de que uma recebe uma lista de inteiros e a outra uma lista de strings
     for (int i = 0; i < tamanho; i++) {
         if (array[i] == elemento) {
             return i;
@@ -28,14 +28,14 @@ int main() {
     int index;
     int num;
 
-    std::cout << "Insira o elemento a ser encontrado: ";
+    std::cout << "Insira o elemento a ser encontrado: "; // esse número será procurado na lista criada acima
     std::cin >> num;
 
-    index = localizarArray(nums, tamanho, num);
+    index = localizarArray(nums, tamanho, num); // com essas 3 informações, a função 'localizarArray' é chamada para encontrar a posição de tal número na lista
 
-    if (index != -1) {
+    if (index != -1) { // se o número for encontrado, sendo o retorno da função localizarArray diferente de -1, a posição dele na lista é exibida
         std::cout << num << " está no índex " << index << '\n';
-    } else {
+    } else { // caso contrário, é exibido que o número não está na lista
         std::cout << num << " não está na lista.\n";
     }
 
@@ -44,10 +44,10 @@ int main() {
     int index2;
     std::string minhaComida;
 
-    std::cout << "Insira a comida que deseja: ";
+    std::cout << "Insira a comida que deseja: "; // o processo é o mesmo para strings, com a diferença de que a lista é de strings
     std::cin >> minhaComida;
 
-    index2 = localizarComida(comidas, tamanho2, minhaComida);
+    index2 = localizarComida(comidas, tamanho2, minhaComida); // e a função 'localizarComida' é chamada para encontrar a posição da comida na lista, da mesma forma que a função 'localizarArray'
 
     if (index2 != -1) {
         std::cout << minhaComida << " está no índex " << index2 << '\n';
