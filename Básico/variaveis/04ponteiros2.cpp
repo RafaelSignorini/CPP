791c48992bbc9ef1e5a1c7fe47749097ecb7d937
Senhor_Ini <rafabs26126@gmail.com>
2025-01-28 12:20:01 -0300

    Create 04ponteiros2.cpp

---
+++ Básico/variaveis/04ponteiros2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <windows.h>
+
+int main() {
+    using std::cout;
+
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    int *ponteiro = nullptr;
+    int x = 987;
+
+    cout << ponteiro; // O ponteiro atualmente tem o valor 0
+
+    ponteiro = &x; // O ponteiro agora aponta para o endereço de x
+
+    if (ponteiro == nullptr) {
+        cout << "O ponteiro não foi inicializado\n";
+    } else {
+        cout << "O ponteiro foi inicializado\n";
+    }
+    return 0;
+}
