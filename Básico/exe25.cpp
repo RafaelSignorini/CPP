6bb23822d5246e34514e629055af3c773a8aaafe
Senhor_Ini <rafabs26126@gmail.com>
2025-01-28 11:21:30 -0300

    Update exe25.cpp

--- Básico/exe25.cpp
+++ Básico/exe25.cpp
@@ -11,13 +11,13 @@ int main() {
     std::cout << carros[1] << '\n';
     std::cout << carros[2] << '\n';
 
-    carros[0] = "Zafira";       // Substitue ou reatribui o valor na primeira posição da lista
+    carros[0] = "Zafira"; // substitue ou reatribui o valor na primeira posição da lista
 
     std::cout << carros[0] << '\n';
 
-    std::string nomes[5];       // Caso a variável seja declarada porém não seja atribuída nenhum valor de imediato, um limite de espaços deve ser declarado nas colchetes
+    std::string nomes[5]; // caso a variável seja declarada porém não seja atribuída nenhum valor de imediato, um limite de espaços deve ser declarado nas colchetes
 
-    nomes[0] = "Rafael Brito Signorini\n";        // Esse é o mais próximo do 'append' que se tem no C++
+    nomes[0] = "Rafael Brito Signorini\n"; // esse é o mais próximo do 'append' que se tem no C++
 
     std::cout << nomes[0];
 
