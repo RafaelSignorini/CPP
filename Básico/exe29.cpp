72ecc3b91ab67a886f31d6e5e7bc63bc3653ac07
Senhor_Ini <rafabs26126@gmail.com>
2025-01-28 11:59:12 -0300

    Update exe29.cpp

--- Básico/exe29.cpp
+++ Básico/exe29.cpp
@@ -7,13 +7,13 @@ int main() {
     
     std::string comidas[5];
     int tamanho = sizeof(comidas)/sizeof(std::string);
-    std::string memo;
+    std::string memo; // essa variável serve como "armazenamento temporário" para as comidas que serão inseridas
 
     std::cout << "Insira uma comida que você gosta, caso queira interromper o código, insira '.' como resposta:\n";
     for (int i = 0; i < tamanho; i++) {
         std::cout << "#" << i + 1 << " ";
         std::getline(std::cin, memo);
-        if (memo == ".") {
+        if (memo == ".") { // se o usuário inserir '.', o código é interrompido, encurtando a lista de comidas
             break;
         } else {
             comidas[i] = memo;
@@ -21,9 +21,9 @@ int main() {
     }
 
     std::cout << "Suas comidas favoritas registradas foram:\n";
-    for (int i = 0; !comidas[i].empty(); i++) {
+    for (int i = 0; !comidas[i].empty(); i++) { // o loop é interrompido quando a string na posição i estiver vazia, no caso o momento que o usuário inserir '.' como resposta
         std::cout << "-=-" << comidas[i] << '\n';
-    }
+    } // esse é um método efiiciente para criar listas de tamanho variável, economizando memória, dando o nome desse método de "lista dinâmica", ou "memória dinâmica"
 
     return 0;
 }
