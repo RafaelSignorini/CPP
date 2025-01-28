c81a1b429caf2c2e9691405b5b8fc4acea566e8f
Senhor_Ini <rafabs26126@gmail.com>
2025-01-28 12:02:40 -0300

    Update exe30.cpp

--- Básico/exe30.cpp
+++ Básico/exe30.cpp
@@ -7,8 +7,8 @@ int main() {
 
     int notas[] = {16, 97, 52, 66};
 
-    for (int nota : notas) {
-        std::cout << nota << '\n';
+    for (int nota : notas) { // esse é um laço for-each, ele percorre todos os elementos de um array já criando uma variável para armazenar o valor
+        std::cout << nota << '\n'; // é um método mais simples de percorrer arrays, mas não é possível acessar o índice do array
     }
     return 0;
 }
