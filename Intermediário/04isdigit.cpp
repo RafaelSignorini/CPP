90cbf05437410932d98596053f213abe71a8b11f
Senhor_Ini <rafabs26126@gmail.com>
2025-02-11 15:17:40 -0300

    Update 04isdigit.cpp

--- Intermediário/04isdigit.cpp
+++ Intermediário/04isdigit.cpp
@@ -5,16 +5,16 @@ int main() {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    char frase{};
+    std::string frase;
     int crc_count = 0;
     std::cout << "Insira uma frase:\n";
-    std::cin >> frase;
+    std::getline(std::cin, frase);
 
     for (auto caractere : frase) {
-        if (std::isdigit(caractere)) {
+        if (std::isdigit(static_cast<unsigned char>(caractere))) { // Converte caractere para unsigned char para evitar comportamento indefinido
             crc_count++;
         }
     }
-    std::cout << "A frase inserida contém " << crc_count << " dígitos registrados.";
+    std::cout << "A frase inserida contém " << crc_count << " números registrados.";
     return 0;
 }
