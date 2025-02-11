537c8385267ffda9db88797ee48eccfb5f51421d
Senhor_Ini <rafabs26126@gmail.com>
2025-02-11 15:22:36 -0300

    Update 04isdigit.cpp

--- Intermediário/04isdigit.cpp
+++ Intermediário/04isdigit.cpp
@@ -7,14 +7,18 @@ int main() {
 
     std::string user_input;
     int crc_count = 0;
-    std::cout << "Insira uma user_input:\n";
+    int num_count = 0;
+    std::cout << "Insira dígitos aleatórios do teclado:\n";
     std::getline(std::cin, user_input);
 
     for (auto caractere : user_input) {
         if (std::isdigit(static_cast<unsigned char>(caractere))) { // Converte caractere para unsigned char para evitar comportamento indefinido
+            num_count++;
+        } else if (std::isalpha(caractere)) {
             crc_count++;
         }
     }
-    std::cout << "Dos valores inseridos, " << crc_count << " são numéricos.";
+    std::cout << "Dos valores inseridos, " << num_count << " são numéricos ";
+    std::cout << "enquanto " << crc_count << " são alfabéticos";
     return 0;
 }
