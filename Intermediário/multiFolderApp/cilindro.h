78f286817f55bfa563b996aeb02814ce2886d354
Senhor_Ini <rafabs26126@gmail.com>
2025-02-18 18:45:26 -0300

    essa bosta não funciona

--- Intermediário/multiFolderApp/cilindro.h
+++ Intermediário/multiFolderApp/cilindro.h
@@ -5,23 +5,10 @@ class Cilindro {
     double altura{1.0};
 public:
     Cilindro() = default;
-    Cilindro(double param_raio, double param_altura) {
-        raio = param_raio;
-        altura = param_altura;
-    }
-    double volume() {
-        return PI * raio * raio * altura;
-    }
-    double getRaio() {
-        return raio;
-    }
-    double getAltura() {
-        return altura;
-    }
-    void setRaio(double param_raio) {
-        raio = param_raio;
-    }
-    void setAltura(double param_altura) {
-        altura = param_altura;
-    }
+    Cilindro(double param_raio, double param_altura);
+    double volume();
+    double getRaio();
+    double getAltura();
+    void setRaio(double param_raio);
+    void setAltura(double param_altura);
 };
