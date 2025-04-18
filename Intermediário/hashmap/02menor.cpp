895e575d908a5e36de313208e1ec9b65e64773ba
Senhor_Ini <rafabs26126@gmail.com>
2025-04-17 22:50:10 -0300

    Create 02menor.cpp

---
+++ Intermediário/hashmap/02menor.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+#include <windows.h>
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    srand(time(NULL));
+
+    std::vector<int> randomNumbers;
+
+    for (int i = 0; i < 12; i++) {
+        int number = std::rand() % 100 - 50;
+        randomNumbers.push_back(number);
+        std::cout << number << " ";
+    }
+
+    int menor = std::numeric_limits<int>::min();
+
+    for (int num : randomNumbers) {
+        if (num < menor) {
+            menor = num;
+        }
+    }
+
+    std::cout << "O menor número é " << menor << std::endl;
+    return 0;
+}
