387dc789e03e08bb74b54c0f214ccb1e1efaa688
Senhor_Ini <rafabs26126@gmail.com>
2025-04-22 00:05:02 -0300

    Update 01helloworld.cpp

--- Intermediário/functions/01helloworld.cpp
+++ Intermediário/functions/01helloworld.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <windows.h>
+#include <vector>
 
 void msg(std::string msg) {
     std::cout << msg << std::endl;
@@ -9,11 +10,25 @@ void helloworld() {
     msg("Olá meu amor <3!");
 }
 
+std::vector<int> gerarNumerosAleatorios(int qnt) {
+    std::vector<int> randomNumbers; // essa variável é local
+
+    std::cout << "Números gerados aleatoriamente:" << std::endl;
+    for (int i = 0; i < 12; i++) {
+        int number = std::rand() % 100 - 50;
+        randomNumbers.push_back(number);
+        std::cout << number << " ";
+    }
+    return randomNumbers;
+}
+
 int main() {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
     helloworld();
+
+    std::vector<int> randomNumbers = gerarNumerosAleatorios(6); // essa variável é global
 
     return 0;
 }
