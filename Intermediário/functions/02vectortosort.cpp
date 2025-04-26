f1da2052670c8a82a89eca8aba67385069fdf0d2
Senhor_Ini <rafabs26126@gmail.com>
2025-04-26 00:03:30 -0300

    Update 02vectortosort.cpp

--- Intermediário/functions/02vectortosort.cpp
+++ Intermediário/functions/02vectortosort.cpp
@@ -3,44 +3,46 @@
 #include <vector>
 #include <ctime>
 
-std::vector<int> generateRandomNumbers(int qnt) {
-    std::srand(time(nullptr));
+std::vector<int> generateRandomNumbers(int qnt) { // int qnt é o parâmetro da quantidade de valores a serem gerados
+    std::srand(time(nullptr)); // gera números verdadeiramente aleatórios, impossibilitando números pseudoaleatórios
     std::vector<int> randomNumbers;
 
     std::cout << "Números gerados aleatoriamente:" << std::endl;
-    for (int i = 0; i < 12; i++) {
-        int number = std::rand() % 100 - 50;
-        randomNumbers.push_back(number);
+    for (int i = 0; i < qnt; i++) {
+        int number = std::rand() % 100 - 50; // gera números de -50 a 49
+        randomNumbers.push_back(number); // adiciona na última posição do vetor
         std::cout << number << " ";
     }
     return randomNumbers;
 }
 
-std::vector<int> sortVector(std::vector<int>& vectorToSort) {
+void sortVector(std::vector<int>& vectorToSort) { // cria a variável vectorToSort e utiliza os valores como referência (&) assim evitando sobreuso de memória
     for (int i = 0; i < vectorToSort.size(); i++) {
         for (int j = 0; j < vectorToSort.size(); j++) {
             if (vectorToSort[i] < vectorToSort[j]) {
-                std::swap(vectorToSort[i], vectorToSort[j]);
+                std::swap(vectorToSort[i], vectorToSort[j]); // organiza em ordem crescente os valores do vetor
             }
         }
     }
-    return vectorToSort;
+}
+
+void printVector(std::vector<int>& vectorToPrint) { // cria outra referência da variável 'randomNumbers'
+    std::cout << "Números em ordem crescente:" << std::endl;
+    for (int num : vectorToPrint) {
+        std::cout << num << " ";
+    }
 }
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    std::vector<int> randomNumbers = generateRandomNumbers(5);
+    std::vector<int> randomNumbers = generateRandomNumbers(7); // cria o vetor que conterá os valores aleatoriamente e recebe seu tamanho como parâmetro da função 'generateRandomNumbers()'
 
     std::cout << "\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-" << std::endl;
 
     sortVector(randomNumbers);
-
-    std::cout << "Números em ordem crescente:" << std::endl;
-    for (int num : randomNumbers) {
-        std::cout << num << " ";
-    }
+    printVector(randomNumbers);
 
     return 0;
 }
